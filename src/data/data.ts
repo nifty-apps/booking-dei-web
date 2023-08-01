601cf9099532f474369176ea409788b9ce4982af
mahabur <annur@niftyitsolution.com>
2023-08-01 16:57:37 +0600

    Hotel room customized

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,112 +1,224 @@
 // hotel room number
 export const roomNumberFirstFloor = [
-  { id: 1, roomNumber: 102, roomType: `Honeymoon Suite (AC)` },
-  { id: 2, roomNumber: 101, roomType: `Family Deluxe Suite (AC)` },
-  { id: 3, lift: `LIFT  ` },
-  { id: 4, roomNumber: 103, roomType: `Super Deluxe Couple (AC)` },
+  {
+    id: 1,
+    roomNumber: 102,
+    roomType: `Honeymoon Suite (AC)`,
+    bg: "rgba(40, 199, 111, 0.16)",
+  },
+  {
+    id: 2,
+    roomNumber: 101,
+    roomType: `Family Deluxe Suite (AC)`,
+    bg: "#fff",
+  },
+  { id: 3, lift: `LIFT `, bg: "#fff" },
+  {
+    id: 4,
+    roomNumber: 103,
+    roomType: `Super Deluxe Couple (AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
+  },
   {
     id: 5,
     roomNumber: 105,
     roomType: `Super Deluxe Triple
 (Non-AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
   },
-  { id: 6, roomNumber: 104, roomType: `Super Deluxe Couple (AC)` },
-  { id: 7, roomNumber: 107, roomType: `Super Deluxe Twin Couple (AC)` },
-  { id: 9, roomNumber: 106, roomType: `Super Deluxe Triple (AC)` },
+  {
+    id: 6,
+    roomNumber: 104,
+    roomType: `Super Deluxe Couple (AC)`,
+    bg: "rgba(40, 199, 111, 0.16)",
+  },
+  {
+    id: 7,
+    roomNumber: 107,
+    roomType: `Super Deluxe Twin Couple (AC)`,
+    bg: "rgba(40, 199, 111, 0.16)",
+  },
+  {
+    id: 9,
+    roomNumber: 106,
+    roomType: `Super Deluxe Triple (AC)`,
+    bg: "rgba(234, 84, 85, 0.16)",
+  },
   {
     id: 10,
     roomNumber: 109,
     roomType: `Super Deluxe Couple
 (Non-AC)`,
+    bg: "rgba(168, 170, 174, 0.16)",
   },
   {
     id: 10,
     roomNumber: 108,
     roomType: `Super Deluxe Couple
 (Non-AC)`,
+    bg: "#fff",
   },
 ];
 
 export const roomNumberSecondFloor = [
-  { id: 1, roomNumber: 202, roomType: `Honeymoon Suite (AC)` },
-  { id: 2, roomNumber: 201, roomType: `Family Deluxe Suite (AC)` },
-  { id: 3, lift: `LIFT  ` },
-  { id: 4, roomNumber: 203, roomType: `Super Deluxe Couple (AC)` },
+  {
+    id: 1,
+    roomNumber: 202,
+    roomType: `Honeymoon Suite (AC)`,
+    bg: "rgba(40, 199, 111, 0.16)",
+  },
+  {
+    id: 2,
+    roomNumber: 201,
+    roomType: `Family Deluxe Suite (AC)`,
+    bg: "rgba(168, 170, 174, 0.16)",
+  },
+  { id: 3, lift: `LIFT `, bg: "#fff" },
+  { id: 4, roomNumber: 203, roomType: `Super Deluxe Couple (AC)`, bg: "#fff" },
   {
     id: 5,
     roomNumber: 205,
     roomType: `Super Deluxe Triple
 (Non-AC)`,
+    bg: "#fff",
   },
-  { id: 6, roomNumber: 204, roomType: `Super Deluxe Couple (AC)` },
-  { id: 7, roomNumber: 207, roomType: `Super Deluxe Twin Couple (AC)` },
-  { id: 9, roomNumber: 206, roomType: `Super Deluxe Triple (AC)` },
+  {
+    id: 6,
+    roomNumber: 204,
+    roomType: `Super Deluxe Couple (AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
+  },
+  {
+    id: 7,
+    roomNumber: 207,
+    roomType: `Super Deluxe Twin Couple (AC)`,
+    bg: "#fff",
+  },
+  {
+    id: 9,
+    roomNumber: 206,
+    roomType: `Super Deluxe Triple (AC)`,
+    bg: "#0059AA",
+  },
   {
     id: 10,
     roomNumber: 209,
     roomType: `Super Deluxe Couple
 (Non-AC)`,
+    bg: "rgba(255, 159, 67, 0.16)",
   },
   {
     id: 10,
     roomNumber: 208,
     roomType: `Super Deluxe Couple
 (Non-AC)`,
+    bg: "rgba(40, 199, 111, 0.16)",
   },
 ];
 
 export const roomNumberThirdFloor = [
-  { id: 1, roomNumber: 302, roomType: `Honeymoon Suite (AC)` },
-  { id: 2, roomNumber: 301, roomType: `Family Deluxe Suite (AC)` },
-  { id: 3, lift: `LIFT  ` },
-  { id: 4, roomNumber: 303, roomType: `Super Deluxe Couple (AC)` },
+  {
+    id: 1,
+    roomNumber: 302,
+    roomType: `Honeymoon Suite (AC)`,
+    bg: "rgba(255, 159, 67, 0.16)",
+  },
+  {
+    id: 2,
+    roomNumber: 301,
+    roomType: `Family Deluxe Suite (AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
+  },
+  { id: 3, lift: `LIFT` },
+  {
+    id: 4,
+    roomNumber: 303,
+    roomType: `Super Deluxe Couple (AC)`,
+    bg: "rgba(234, 84, 85, 0.16)",
+  },
   {
     id: 5,
     roomNumber: 305,
     roomType: `Super Deluxe Triple
 (Non-AC)`,
+    bg: "rgba(234, 84, 85, 0.16)",
   },
   { id: 6, roomNumber: 304, roomType: `Super Deluxe Couple (AC)` },
-  { id: 7, roomNumber: 307, roomType: `Super Deluxe Twin Couple (AC)` },
+  {
+    id: 7,
+    roomNumber: 307,
+    roomType: `Super Deluxe Twin Couple (AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
+  },
   { id: 9, roomNumber: 306, roomType: `Super Deluxe Triple (AC)` },
   {
     id: 10,
     roomNumber: 309,
     roomType: `Super Deluxe Couple
 (Non-AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
   },
   {
     id: 10,
     roomNumber: 308,
     roomType: `Super Deluxe Couple
 (Non-AC)`,
+    bg: "rgba(255, 159, 67, 0.16)",
   },
 ];
 
 export const roomNumberFourthFloor = [
-  { id: 1, roomNumber: 402, roomType: `Honeymoon Suite (AC)` },
+  {
+    id: 1,
+    roomNumber: 402,
+    roomType: `Honeymoon Suite (AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
+  },
   { id: 2, roomNumber: 401, roomType: `Family Deluxe Suite (AC)` },
-  { id: 3, lift: `LIFT  ` },
-  { id: 4, roomNumber: 403, roomType: `Super Deluxe Couple (AC)` },
+  { id: 3, lift: `LIFT `, bg: "#fff" },
+  {
+    id: 4,
+    roomNumber: 403,
+    roomType: `Super Deluxe Couple (AC)`,
+    bg: "rgba(255, 159, 67, 0.16)",
+  },
   {
     id: 5,
     roomNumber: 405,
     roomType: `Super Deluxe Triple
 (Non-AC)`,
+    bg: "rgba(255, 159, 67, 0.16)",
   },
-  { id: 6, roomNumber: 404, roomType: `Super Deluxe Couple (AC)` },
-  { id: 7, roomNumber: 407, roomType: `Super Deluxe Twin Couple (AC)` },
-  { id: 9, roomNumber: 406, roomType: `Super Deluxe Triple (AC)` },
+  {
+    id: 6,
+    roomNumber: 404,
+    roomType: `Super Deluxe Couple (AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
+  },
+  {
+    id: 7,
+    roomNumber: 407,
+    roomType: `Super Deluxe Twin Couple (AC)`,
+    bg: "#fff",
+  },
+  {
+    id: 9,
+    roomNumber: 406,
+    roomType: `Super Deluxe Triple (AC)`,
+    bg: "rgba(40, 199, 111, 0.16)",
+  },
   {
     id: 10,
     roomNumber: 409,
     roomType: `Super Deluxe Couple
 (Non-AC)`,
+    bg: "rgba(0, 89, 170, 0.16)",
   },
   {
     id: 10,
     roomNumber: 408,
     roomType: `Super Deluxe Couple
 (Non-AC)`,
+    bg: "#fff",
   },
 ];
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,16 +12,21 @@ const Home = () => {
     <>
       <div className="grid grid-cols-12">
         <div className="col-span-9">
+          {/* title */}
+          <h2 className="text-3xl font-semibold text-gray-600 my-4">Home</h2>
           <div className="grid grid-cols-4 gap-2">
             <div className=" bg-gray-100">
-              <div className="capitalize">First floor</div>
+              <div className="capitalize border text-center py-3 bg-white rounded-lg border-gray-300 text-black font-semibold">
+                First floor
+              </div>
               <div className="grid grid-cols-2 gap-2">
                 {roomNumberFirstFloor.map((room) => {
-                  const { id, roomNumber, lift, roomType } = room;
+                  const { id, roomNumber, lift, roomType, bg } = room;
                   return (
                     <div
                       key={id}
-                      className="w-full bg-white rounded-lg shadow-sm p-2 border border-gray-300"
+                      className={`w-full rounded-lg shadow-sm p-2 border border-gray-300`}
+                      style={{ background: bg }}
                     >
                       <h4>{roomNumber}</h4>
                       <p>{lift}</p>
@@ -32,14 +37,17 @@ const Home = () => {
               </div>
             </div>
             <div className=" bg-gray-100">
-              <div>Second floor</div>
+              <div className="capitalize border text-center py-3 bg-white rounded-lg border-gray-300 text-black font-semibold">
+                Second floor
+              </div>
               <div className="grid grid-cols-2 gap-2">
                 {roomNumberSecondFloor.map((room) => {
-                  const { id, roomNumber, lift, roomType } = room;
+                  const { id, roomNumber, lift, roomType, bg } = room;
                   return (
                     <div
                       key={id}
                       className="w-full bg-white rounded-lg shadow-sm p-2 border border-gray-300"
+                      style={{ background: bg }}
                     >
                       <h4>{roomNumber}</h4>
                       <p>{lift}</p>
@@ -50,14 +58,17 @@ const Home = () => {
               </div>
             </div>
             <div className=" bg-gray-100">
-              <div>Third floor</div>
+              <div className="capitalize border text-center py-3 bg-white rounded-lg border-gray-300 text-black font-semibold">
+                Third floor
+              </div>
               <div className="grid grid-cols-2 gap-2">
                 {roomNumberThirdFloor.map((room) => {
-                  const { id, roomNumber, lift, roomType } = room;
+                  const { id, roomNumber, lift, roomType, bg } = room;
                   return (
                     <div
                       key={id}
                       className="w-full bg-white rounded-lg shadow-sm p-2 border border-gray-300"
+                      style={{ background: bg }}
                     >
                       <h4>{roomNumber}</h4>
                       <p>{lift}</p>
@@ -68,14 +79,17 @@ const Home = () => {
               </div>
             </div>
             <div className=" bg-gray-100">
-              <div>Fourth floor</div>
+              <div className="capitalize border text-center py-3 bg-white rounded-lg border-gray-300 text-black font-semibold">
+                Fourth floor
+              </div>
               <div className="grid grid-cols-2 gap-2">
                 {roomNumberFourthFloor.map((room) => {
-                  const { id, roomNumber, lift, roomType } = room;
+                  const { id, roomNumber, lift, roomType, bg } = room;
                   return (
                     <div
                       key={id}
                       className="w-full bg-white rounded-lg shadow-sm p-2 border border-gray-300"
+                      style={{ background: bg }}
                     >
                       <h4>{roomNumber}</h4>
                       <p>{lift}</p>
