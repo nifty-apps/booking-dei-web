f4030958c7e6e3638fe5df9da8d8583b80f37bdd
mahabur <annur@niftyitsolution.com>
2023-08-02 18:02:23 +0600

    new booking flow UI  customize

--- src/pages/NewBooking/NewBooking.tsx
+++ src/pages/NewBooking/NewBooking.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { DatePicker, Modal, Select, Checkbox, Form, Input, Button } from "antd";
 // data
 
-import { AiOutlineDown } from "react-icons/ai";
+import { AiOutlineClose, AiOutlineDown } from "react-icons/ai";
 import { FaPlus } from "react-icons/fa";
 import RoomNumber from "../../components/RoomNumber";
 import { BsThreeDotsVertical } from "react-icons/bs";
@@ -239,35 +239,97 @@ const NewBooking = () => {
 
         {/* booking summary */}
         <div className="col-span-3 bg-gray-200 p-4 rounded-sm ml-4">
-          <h3 className="text-lg capitalize font-semibold mb-5">
-            Booking Summary
-          </h3>
-          <hr />
-          <p>Subtotal: </p>
-          <p>Discount</p>
-          <hr />
+          <div className="col-span-3 bg-gray-200 p-4 rounded-sm ml-4">
+            <h3 className="text-lg capitalize font-semibold mb-5">
+              Booking Summary
+            </h3>
+            <div className="flex justify-between text-md">
+              <div className="flex items-center">
+                <span className="mr-2">1.</span>
+                <span className="cursor-pointer">
+                  <AiOutlineClose />
+                </span>
+                <div className="mx-2 font-semibold">
+                  Super Deluxe Tripel (Non-AC)
+                </div>
+              </div>
+              <div className="font-semibold">5000.00</div>
+            </div>
+            <div className="flex items-center justify-between">
+              <p>Extrans Bed + Breakfast</p>
+              <p>1000.00</p>
+            </div>
 
-          <p>Grand Total: </p>
-          <p>Inclusive of 15% value added tas (vat)</p>
+            <div className="flex justify-between text-md mt-2">
+              <div className="flex items-center">
+                <span className="mr-2">1.</span>
+                <span className="cursor-pointer">
+                  <AiOutlineClose />
+                </span>
+                <div className="mx-2 font-semibold">
+                  Super Deluxe Tripel (Non-AC)
+                </div>
+              </div>
+              <div className="font-semibold">10000.00</div>
+            </div>
+            <div className="border border-gray-400 my-2"></div>
 
-          <div>
-            <input type="text" placeholder="Enter Discount" />
-            <button>Apply</button>
-          </div>
+            <div className="flex items-center justify-between">
+              <p className="font-bold">Subtotal</p>
+              <p>16000.00</p>
+            </div>
 
-          {/* total payments */}
-          <p>Total: </p>
+            <div className="flex items-center justify-between">
+              <p className="font-bold">Grand Total</p>
+              <p>16000.00</p>
+            </div>
 
-          <hr />
+            <div className="border border-gray-400 my-2"></div>
+            <div className="flex items-center justify-between">
+              <p className="font-bold">Grand Total</p>
+              <p>16000.00</p>
+            </div>
 
-          <p>Due</p>
+            <p className="text-gray-400 text-xs">
+              Inclusive of 15% Value Added Tax (VAT)
+            </p>
 
-          {/* payment btn  */}
-          <div className="mt-72">
-            <h1>Payments</h1>
-            <button className="bg-blue-900 text-white px-4 py-2 rounded-md w-full mb-2 font-semibold">
-              + New Payment
-            </button>
+            {/* Discount field */}
+            <div className="mt-4 mb-14">
+              <input
+                type="text"
+                placeholder="1000.00"
+                className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+              />
+              <button className="hover:bg-blue-600 text-blue-700 font-bold py-2 px-4 border border-blue-700">
+                Apply
+              </button>
+            </div>
+
+            <div>
+              {/* payments */}
+              <h1 className="font-semibold text-xl text-black mb-4 capitalize pb-24">
+                Payments
+              </h1>
+
+              <div>
+                <div className="border border-gray-400 my-2"></div>
+                <div className="flex items-center justify-between">
+                  <p className="font-bold">Due</p>
+                  <p>16000.00</p>
+                </div>
+              </div>
+            </div>
+
+            {/* payment btn  */}
+            <div className="mt-32">
+              <button className="bg-blue-900 text-white px-4 py-2 rounded-md w-full mb-2 font-semibold flex items-center justify-center gap-2">
+                <span>
+                  <FaPlus />
+                </span>
+                New Payment
+              </button>
+            </div>
           </div>
         </div>
       </div>
