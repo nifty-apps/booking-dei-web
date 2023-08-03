745767b21e71fa2731659dd5f1e8042378fe5dc4
mahabur <annur@niftyitsolution.com>
2023-08-03 11:46:31 +0600

    room number UI customized by padding

--- src/components/RoomNumber.tsx
+++ src/components/RoomNumber.tsx
@@ -8,7 +8,7 @@ import {
 const RoomNumber = () => {
   return (
     <>
-      <div className="col-span-9">
+      <div className="col-span-9 bg-white p-8 rounded">
         {/* room number part */}
         <div className="grid grid-cols-4 gap-2">
           <div className=" bg-gray-100">
@@ -104,7 +104,6 @@ const RoomNumber = () => {
             </div>
           </div>
         </div>
-
         {/* rounding circle */}
         <div className="flex items-center justify-center gap-2 mt-3">
           <div className="w-5 h-5 rounded-full bg-white border border-gray-500"></div>
