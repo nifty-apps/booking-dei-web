2eafdb242a58bd16dba70373ba9d0f86eb243dc0
mahabur <annur@niftyitsolution.com>
2023-08-09 13:09:25 +0600

    🔧 chore(Login.tsx): refactor Login component to improve code structure and styling 🎨 style(Login.tsx): update form layout and styling for better user experience

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,73 +1,66 @@
 import { Button, Checkbox, Form, Input } from "antd";
 
-
 const onFinish = () => {};
 const onFinishFailed = () => {};
 
-const App = () => (
-  <Form
-    name="basic"
-    labelCol={{
-      span: 8,
-    }}
-    wrapperCol={{
-      span: 16,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[
-        {
-          required: true,
-          message: "Please input your username!",
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: "Please input your password!",
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
-
-    <Form.Item
-      name="remember"
-      valuePropName="checked"
+const Login = () => (
+  <div className="flex items-center justify-center h-[500px]">
+    <Form
+      className="margin-auto w-[400px]"
+      name="login"
+      labelCol={{
+        span: 8,
+      }}
       wrapperCol={{
-        offset: 8,
         span: 16,
       }}
-    >
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
-
-    <Form.Item
-      wrapperCol={{
-        offset: 8,
-        span: 16,
+      initialValues={{
+        remember: true,
       }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
     >
-      <Button htmlType="submit">Submit</Button>
-    </Form.Item>
-  </Form>
+      <Form.Item
+        label="Phone"
+        name="phone"
+        rules={[
+          {
+            required: true,
+            message: "Enter phone number!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Enter password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <button
+          type="submit"
+          className="w-full mt-2 bg-blue-600 hover:bg-blue-500 text-white font-semibold py-2 px-4 rounded"
+        >
+          Submit
+        </button>
+      </Form.Item>
+    </Form>
+  </div>
 );
-export default App;
+export default Login;
