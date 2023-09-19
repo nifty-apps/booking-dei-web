873d7f1ecb7a2a5d4a71b724559429c9160fd930
mahabur <annur@niftyitsolution.com>
2023-09-19 17:59:08 +0600

    🎉 feat(RoomBookingFinancials.tsx): add RoomBookingFinancials component to display room booking financial information
    The RoomBookingFinancials component is added to display financial information related to room bookings. It fetches data from the server using the GET_ROOM_BOOKING_FINANCIALS query and displays it in a table format.
    
    The component includes the following features:
    - A search input field to filter the data based on room number, contact, booking amount, paid amount, and due amount.
    - A date picker to select a specific date for filtering the data.
    - The table displays the room number, contact, booking amount, paid amount, due amount, and a link to view booking details for each transaction.
    - The component is connected to the Redux store to access the authenticated user's information.
    
    This component provides a convenient way for users to view and analyze the financial information related to room bookings.

---
+++ src/pages/RoomBookingFinancials/RoomBookingFinancials.tsx
@@ -0,0 +1,121 @@
+import { useQuery } from "@apollo/client";
+import { DatePicker, Input, Table } from "antd";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import TitleText from "../../components/Title";
+import { GET_ROOM_BOOKING_FINANCIALS } from "../../graphql/queries/roomBookingFinancialQueries";
+import { RootState } from "../../store";
+
+const RoomBookingFinancials = () => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [searchText, setSearchText] = useState("");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const { data, loading, error } = useQuery(GET_ROOM_BOOKING_FINANCIALS, {
+    variables: {
+      hotel: user?.hotels[0] || "",
+      startDate: selectedDate,
+      endDate: selectedDate,
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error occured - {error.message}</p>;
+
+  const columns = [
+    {
+      title: "ROOM NUMBER",
+      dataIndex: "roomNumber",
+      key: "roomNumber",
+    },
+    {
+      title: "CONTACT",
+      dataIndex: "contact",
+      key: "contact",
+    },
+    {
+      title: "BOOKING AMOUNT",
+      dataIndex: "bookingAmount",
+      key: "bookingAmount",
+    },
+    {
+      title: "PAID AMOUNT",
+      dataIndex: "paidAmount",
+      key: "paidAmount",
+    },
+    {
+      title: "DUE AMOUNT",
+      dataIndex: "dueAmount",
+      key: "dueAmount",
+    },
+    {
+      title: "ACTION",
+      dataIndex: "action",
+      key: "action",
+      render: (bookingId: string) => (
+        <Link to={`/booking-details/${bookingId}`} className="text-blue-600">
+          Booking Details
+        </Link>
+      ),
+    },
+  ];
+
+  const dataSource = data?.roomBookingFinancials
+    .filter((transaction) => transaction?.roombookings?.length > 0)
+    .filter((transaction) => {
+      const lowerCaseQuery = searchText.toLowerCase();
+      return (
+        (transaction?.number?.toString()?.includes(lowerCaseQuery) ?? false) ||
+        (transaction?.roombookings?.[0]?.bookingCustomer
+          ?.toLowerCase()
+          .includes(lowerCaseQuery) ??
+          false) ||
+        (transaction?.type?.rent?.toString()?.includes(lowerCaseQuery) ??
+          false) ||
+        (transaction?.roombookings?.[0]?.bookingPayment
+          ?.toString()
+          .includes(lowerCaseQuery) ??
+          false) ||
+        (transaction?.roombookings?.[0]?.bookingDue
+          ?.toString()
+          .includes(lowerCaseQuery) ??
+          false)
+      );
+    })
+    .map((transaction) => ({
+      key: transaction?._id,
+      roomNumber: transaction?.number,
+      contact: transaction?.roombookings?.[0]?.bookingCustomer,
+      bookingAmount: transaction?.type?.rent,
+      paidAmount: transaction?.roombookings?.[0]?.bookingPayment,
+      dueAmount: transaction?.roombookings?.[0]?.bookingDue,
+      action: transaction?.roombookings?.[0]?.booking,
+    }));
+
+  return (
+    <>
+      <div className="mb-5">
+        <TitleText text="Rooms Overview" />
+      </div>
+      <div className="flex align-middle justify-between mb-3">
+        <div className="w-3/12">
+          <Input
+            placeholder="Search here.."
+            allowClear
+            size="middle"
+            onChange={(event) => setSearchText(event.target.value)}
+          />
+        </div>
+        <DatePicker
+          allowClear={false}
+          placeholder="Select Date"
+          onChange={(_, date) => setSelectedDate(new Date(date))}
+        />
+      </div>
+      <Table dataSource={dataSource} columns={columns} />
+    </>
+  );
+};
+
+export default RoomBookingFinancials;
