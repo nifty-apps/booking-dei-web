0f28d1771939b91b8237ff3d0999cd4860e66a81
Ekram Hossain <ekramhossain117@gmail.com>
2023-10-23 14:57:23 +0600

    feat : booking overview page implement and print features implement

--- src/pages/BookingOverview/BookingOverview.tsx
+++ src/pages/BookingOverview/BookingOverview.tsx
@@ -1,136 +1,136 @@
-// import { useQuery } from "@apollo/client";
-// import { Button, DatePicker, Input, Spin, Table } from "antd";
-// import dayjs from "dayjs";
-// import { useEffect, useRef, useState } from "react";
-// import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
-// import TitleText from "../../components/Title";
-// import { GET_ROOM_BOOKING_FINANCIALS } from "../../graphql/queries/roomBookingFinancialQueries";
-// import { RootState } from "../../store";
-// import { PrinterOutlined } from "@ant-design/icons";
-// import ReactToPrint from 'react-to-print';
-// const loadingStyles = {
-//   display: "flex",
-//   justifyContent: "center",
-//   alignItems: "center",
-//   height: "100vh", // Make it full height of the viewport
-// };
+import { useQuery } from "@apollo/client";
+import { Button, DatePicker, Input, Spin, Table } from "antd";
+import dayjs from "dayjs";
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import TitleText from "../../components/Title";
+import { GET_ROOM_BOOKING_FINANCIALS } from "../../graphql/queries/roomBookingFinancialQueries";
+import { RootState } from "../../store";
+import { PrinterOutlined } from "@ant-design/icons";
+import ReactToPrint from 'react-to-print';
+const loadingStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh", // Make it full height of the viewport
+};
 const BookingOverview = () => {
 
-//   const { user } = useSelector((state: RootState) => state.auth);
-//   const componentRef = useRef(null);
+  const { user } = useSelector((state: RootState) => state.auth);
+  const componentRef = useRef(null);
 
-//   const [searchText, setSearchText] = useState("");
-//   const [selectedDate, setSelectedDate] = useState(new Date());
-//   const [formattedDate, setFormattedDate] = useState(
-//     dayjs(selectedDate).format("YYYY-MM-DD")
-//   );
+  const [searchText, setSearchText] = useState("");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [formattedDate, setFormattedDate] = useState(
+    dayjs(selectedDate).format("YYYY-MM-DD")
+  );
 
-//   const { data, loading, error, refetch } = useQuery(
-//     GET_ROOM_BOOKING_FINANCIALS,
-//     {
-//       variables: {
-//         hotel: user?.hotels[0] || "",
-//         startDate: selectedDate,
-//         endDate: selectedDate,
-//       },
-//     }
-//   );
+  const { data, loading, error, refetch } = useQuery(
+    GET_ROOM_BOOKING_FINANCIALS,
+    {
+      variables: {
+        hotel: user?.hotels[0] || "",
+        startDate: selectedDate,
+        endDate: selectedDate,
+      },
+    }
+  );
 
-//   useEffect(() => {
-//     refetch({ startDate: selectedDate, endDate: selectedDate });
-//   }, [refetch, selectedDate]);
+  useEffect(() => {
+    refetch({ startDate: selectedDate, endDate: selectedDate });
+  }, [refetch, selectedDate]);
 
-//   if (loading) return <div style={loadingStyles}>
-//     <Spin size="large" tip="Loading..." style={loadingStyles} />
-//   </div>
-//   if (error) return <p>Error occurred - {error.message}</p>;
+  if (loading) return <div style={loadingStyles}>
+    <Spin size="large" tip="Loading..." style={loadingStyles} />
+  </div>
+  if (error) return <p>Error occurred - {error.message}</p>;
 
-//   const columns = [
-//     {
-//       title: "ROOM NUMBER",
-//       dataIndex: "roomNumber",
-//       key: "roomNumber",
-//     },
-//     {
-//       title: "CONTACT",
-//       dataIndex: "contact",
-//       key: "contact",
-//     },
-//     {
-//       title: "RENT AMOUNT",
-//       dataIndex: "bookingAmount",
-//       key: "bookingAmount",
-//     },
-//     {
-//       title: "PAID AMOUNT",
-//       dataIndex: "paidAmount",
-//       key: "paidAmount",
-//     },
-//     {
-//       title: "DUE AMOUNT",
-//       dataIndex: "dueAmount",
-//       key: "dueAmount",
-//     },
-//     {
-//       title: "ACTION",
-//       dataIndex: "action",
-//       key: "action",
-//       render: (bookingId: string) => (
-//         <Link to={`/booking-details/${bookingId}`} className="text-blue-600">
-//           Booking Details
-//         </Link>
-//       ),
-//     },
-//   ];
+  const columns = [
+    {
+      title: "ROOM NUMBER",
+      dataIndex: "roomNumber",
+      key: "roomNumber",
+    },
+    {
+      title: "CONTACT",
+      dataIndex: "contact",
+      key: "contact",
+    },
+    {
+      title: "RENT AMOUNT",
+      dataIndex: "bookingAmount",
+      key: "bookingAmount",
+    },
+    {
+      title: "PAID AMOUNT",
+      dataIndex: "paidAmount",
+      key: "paidAmount",
+    },
+    {
+      title: "DUE AMOUNT",
+      dataIndex: "dueAmount",
+      key: "dueAmount",
+    },
+    {
+      title: "ACTION",
+      dataIndex: "action",
+      key: "action",
+      render: (bookingId: string) => (
+        <Link to={`/booking-details/${bookingId}`} className="text-blue-600">
+          Booking Details
+        </Link>
+      ),
+    },
+  ];
 
-//   const dataSource = data?.roomBookingFinancials
-//     .filter((transaction) => transaction?.roombookings?.length > 0)
-//     .filter((transaction) => {
-//       const lowerCaseQuery = searchText.toLowerCase();
-//       return (
-//         (transaction?.number?.toString()?.includes(lowerCaseQuery) ?? false) ||
-//         (transaction?.roombookings?.[0]?.bookingCustomer
-//           ?.toLowerCase()
-//           .includes(lowerCaseQuery) ??
-//           false) ||
-//         (transaction?.type?.rent?.toString()?.includes(lowerCaseQuery) ??
-//           false) ||
-//         (transaction?.roombookings?.[0]?.bookingPayment
-//           ?.toString()
-//           .includes(lowerCaseQuery) ??
-//           false) ||
-//         (transaction?.roombookings?.[0]?.bookingDue
-//           ?.toString()
-//           .includes(lowerCaseQuery) ??
-//           false)
-//       );
-//     })
-//     .map((transaction) => ({
-//       key: transaction?._id,
-//       roomNumber: transaction?.number,
-//       contact: transaction?.roombookings?.[0]?.bookingCustomer,
-//       bookingAmount: transaction?.roombookings?.[0]?.bookingRent,
-//       paidAmount: transaction?.roombookings?.[0]?.bookingPayment,
-//       dueAmount: transaction?.roombookings?.[0]?.bookingDue,
-//       action: transaction?.roombookings?.[0]?.booking,
-//     }));
+  const dataSource = data?.roomBookingFinancials
+    .filter((transaction) => transaction?.roombookings?.length > 0)
+    .filter((transaction) => {
+      const lowerCaseQuery = searchText.toLowerCase();
+      return (
+        (transaction?.number?.toString()?.includes(lowerCaseQuery) ?? false) ||
+        (transaction?.roombookings?.[0]?.bookingCustomer
+          ?.toLowerCase()
+          .includes(lowerCaseQuery) ??
+          false) ||
+        (transaction?.type?.rent?.toString()?.includes(lowerCaseQuery) ??
+          false) ||
+        (transaction?.roombookings?.[0]?.bookingPayment
+          ?.toString()
+          .includes(lowerCaseQuery) ??
+          false) ||
+        (transaction?.roombookings?.[0]?.bookingDue
+          ?.toString()
+          .includes(lowerCaseQuery) ??
+          false)
+      );
+    })
+    .map((transaction) => ({
+      key: transaction?._id,
+      roomNumber: transaction?.number,
+      contact: transaction?.roombookings?.[0]?.bookingCustomer,
+      bookingAmount: transaction?.roombookings?.[0]?.bookingRent,
+      paidAmount: transaction?.roombookings?.[0]?.bookingPayment,
+      dueAmount: transaction?.roombookings?.[0]?.bookingDue,
+      action: transaction?.roombookings?.[0]?.booking,
+    }));
 
-//   const handlePreviousClick = () => {
-//     const newDate = new Date(selectedDate);
-//     newDate.setDate(newDate.getDate() - 1);
-//     setSelectedDate(newDate);
-//     setFormattedDate(dayjs(newDate).format("YYYY-MM-DD"));
-//     refetch({ startDate: newDate, endDate: newDate });
-//   };
+  const handlePreviousClick = () => {
+    const newDate = new Date(selectedDate);
+    newDate.setDate(newDate.getDate() - 1);
+    setSelectedDate(newDate);
+    setFormattedDate(dayjs(newDate).format("YYYY-MM-DD"));
+    refetch({ startDate: newDate, endDate: newDate });
+  };
 
-//   const handleNextClick = () => {
-//     const newDate = new Date(selectedDate);
-//     newDate.setDate(newDate.getDate() + 1);
-//     setSelectedDate(newDate);
-//     setFormattedDate(dayjs(newDate).format("YYYY-MM-DD"));
-//     refetch({ startDate: newDate, endDate: newDate });
-  // };
+  const handleNextClick = () => {
+    const newDate = new Date(selectedDate);
+    newDate.setDate(newDate.getDate() + 1);
+    setSelectedDate(newDate);
+    setFormattedDate(dayjs(newDate).format("YYYY-MM-DD"));
+    refetch({ startDate: newDate, endDate: newDate });
+  };
 
 
 
@@ -140,7 +140,7 @@ const BookingOverview = () => {
 
 
     <>
-      {/* <div>
+      <div>
 
 
 
@@ -192,7 +192,7 @@ const BookingOverview = () => {
       </div>
       <div ref={componentRef}>
         <Table dataSource={dataSource} columns={columns} pagination={false} />
-      </div> */}
+      </div>
 
     </>
 
