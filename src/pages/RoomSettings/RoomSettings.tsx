24f660fc2af8a1757e0cea438bd1ed4c34e9deee
Nur Mohammad <mohammadnur5480@gmail.com>
2023-11-08 12:58:17 +0600

    feat: added setting search bar and deactivated button

--- src/pages/RoomSettings/RoomSettings.tsx
+++ src/pages/RoomSettings/RoomSettings.tsx
@@ -1,8 +1,11 @@
+import { Input, Switch, Tooltip } from "antd";
 import TitleText from "../../components/Title";
 import { PlusOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 const RoomSettings = () => {
-
+    const [searchText, setSearchText] = useState<string>("");
+    const [filterDeactivated, setFilterDeactivated] = useState<boolean>(false);
   return (
     <>
       <div className="mb-5 flex justify-between">
@@ -11,6 +14,30 @@ const RoomSettings = () => {
           <PlusOutlined />
           Add Room
         </button>
+      </div>
+      <div className="flex items-center justify-between mb-3">
+        <div className="w-3/12">
+          <Input
+            placeholder="Search here.."
+            allowClear
+            size="middle"
+            onChange={(e) => setSearchText(e.target.value)}
+            value={searchText}
+          />
+        </div>
+
+        <Tooltip
+          title={`See ${filterDeactivated ? "Active" : "Deactivated"} Employees`}
+          placement="bottomRight"
+          className="cursor-pointer flex justify-center gap-1"
+        >
+          <Switch
+            className={`${filterDeactivated ? "" : "bg-gray-400"}`}
+            defaultChecked={false}
+            onChange={() => setFilterDeactivated(!filterDeactivated)}
+          />
+          Deactivated
+        </Tooltip>
       </div>
     </>
   );
