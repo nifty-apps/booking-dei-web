032841478b2de1f662df3c762ef530938262b770
Nur Mohammad <mohammadnur5480@gmail.com>
2023-11-08 12:57:11 +0600

    feat: added room booking title and room add button

--- src/pages/RoomSettings/RoomSettings.tsx
+++ src/pages/RoomSettings/RoomSettings.tsx
@@ -1,6 +1,18 @@
+import TitleText from "../../components/Title";
+import { PlusOutlined } from "@ant-design/icons";
+
 const RoomSettings = () => {
+
   return (
-    <div>RoomSettings</div>
-  )
-}
-export default RoomSettings
+    <>
+      <div className="mb-5 flex justify-between">
+        <TitleText text="Room Settings" />
+        <button className="hover:text-blue-700 px-20 py-2 rounded-md mb-2 font-semibold capitalize flex items-center gap-2 border border-blue-900 bg-blue-700 text-white hover:bg-white duration-200">
+          <PlusOutlined />
+          Add Room
+        </button>
+      </div>
+    </>
+  );
+};
+export default RoomSettings;
